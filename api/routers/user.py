16ddfde1929dff7f11d86b98c16fd18ab737da69
dreamerror <66074257+dreamerror@users.noreply.github.com>
2022-07-31 18:24:55 +0900

    Added OAuth2 Password Bearer

--- api/routers/user.py
+++ api/routers/user.py
@@ -1,16 +1,34 @@
+import bcrypt as bc
+import jwt
 from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordBearer
 from pydantic import EmailStr
 
 from models.account import User
+import api.settings as settings
 
 router = APIRouter(
     prefix="/user",
     tags=["user"]
 )
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/signup")
 
-async def access_level_params(is_superuser: bool):
-    return {"is_superuser": is_superuser}
+
+def decode_jwt(token: str):
+    return jwt.decode(
+        token,
+        settings.JWT_SECRET,
+        algorithms=[settings.JWT_ALGORITHM]
+    )
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = decode_jwt(token)
+    except jwt.PyJWTError:
+        raise jwt.PyJWTError
+    return payload
 
 
 @router.get("/{user_id}")
@@ -18,12 +36,15 @@ async def get_user_by_id(user_id: int):
     return User(email=EmailStr("example@example.com"), hashed_password="example_pass")
 
 
+@router.get("/me")
+async def get_me(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = decode_jwt(token)
+    except jwt.PyJWTError:
+        raise jwt.PyJWTError
+    return payload
+
+
 @router.post("/register", response_model=User, status_code=201)
 async def create_user(user: User):
     return user
-
-
-@router.post("/reg_su", response_model=User, status_code=201)
-async def create_superuser(user: User, params: dict = Depends(access_level_params)):
-    return user if params["is_superuser"] else {"error": "Permission denied"}
-
