2786cd2941317b2719abe87a0a964c747669e36a
dreamerror <66074257+dreamerror@users.noreply.github.com>
2022-08-02 00:23:46 +0900

    Fake database as temporary solution

---
+++ api/fake_db.py
@@ -0,0 +1,13 @@
+from models.account import Company, User
+
+
+users = [
+        {
+            "email": "example@example.com",
+            "hashed_password": "$2b$12$3tuMTnlaJ8zE6pSyUpO9Dej8.xLN215DGWYsuv6aTADueQDMtua2e",  # example
+        },
+        {
+            "email": "nevergonna@giveyou.up",
+            "hashed_password": "$2b$12$aDJVqvO9MSH7l7C8tte/ZuYV.SE7fHYHVMn.2h.Ix7JHPah6fBGda"  # nevergonna
+        }
+]
