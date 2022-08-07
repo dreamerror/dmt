eecf29b963bca238c6eb6008154f9a20ee8b456b
dreamerror <66074257+dreamerror@users.noreply.github.com>
2022-08-07 19:20:50 +0900

    Allow to put documents in Database class instance

--- couchdb/database.py
+++ couchdb/database.py
@@ -1,6 +1,15 @@
+from typing import List
+
+from couchdb.document import Document
+
+
 class Database:
-    def __init__(self, name: str):
+    def __init__(self, name: str, documents: List[Document] | None = None):
         self.name = name
+        self.docs = documents if documents is not None else list()
 
     def __repr__(self):
         return f"Database(\"{self.name}\")"
+
+    def add_doc(self, document: Document):
+        self.docs.append(document)
