dbef15228ffb5b415486c24e5e4dcb75bdf4596e
dreamerror <66074257+dreamerror@users.noreply.github.com>
2022-09-01 20:36:57 +0900

    Fix ConflictingDocument exception on updating document

--- api/routers/admin.py
+++ api/routers/admin.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 
 import settings
-from api.jwt import decode_jwt, get_current_user
+from api.jwt import decode_jwt, user_has_permission
 from api.db import server
 from couchdb import Document
 from couchdb.query import Selector, SelectorElement
@@ -20,8 +20,8 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/signup")
 
 @router.post("/set_admin")
 async def set_user_as_admin(token: str = Depends(oauth2_scheme), username: str = None):
-    payload = await get_current_user(token)
-    if not payload["is_superuser"]:
+    permission = await user_has_permission(token, "superuser")
+    if not permission:
         raise HTTPException(
             status_code=401,
             detail="You have no permission to set user as admin"
@@ -32,6 +32,6 @@ async def set_user_as_admin(token: str = Depends(oauth2_scheme), username: str =
     selector = Selector()
     selector.add_elements(email_selector)
     data = await users_db.find_docs(selector)
-    users_db.create_document(Document(_id=data[0]["_id"], username=username, hashed_pw=data[0]["hashed_pw"],
-                                      is_admin=True))
+    users_db.create_document(Document(_id=data[0]["_id"], _rev=data[0]["_rev"],
+                                      username=username, hashed_pw=data[0]["hashed_pw"], is_admin=True))
     return True
